6f5ddbeff0e8d59d9c1f9a152b62611c7bb5a9a6
mlinskey90 <michael.linskey@sse.com>
2024-05-27 09:35:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def calculate_ballast_and_buoyancy(params, C2, C4, rho_ballast_wet, rho_water, r
     d1, d2, h1, h2, h3, h4, h5 = params[0], params[1], params[2], params[3], params[4], params[5], params[6]
     h_water = h1 + h2 + h3 - h4
     B_wet = ((np.pi * d1**2 / 4) * (h2 + h3 - h4) - (C2) - (np.pi * d2**2 / 4) * (h3 - h4)) * rho_ballast_wet
-    W = ((np.pi * d1**2 / 4) * h_water + (C4 / rho_conc)) * rho_water  # Corrected buoyancy force calculation
+    W = (((np.pi * (d1**2 / 4)) * h_water) + (C4 / rho_conc)) * rho_water  # Corrected buoyancy force calculation
     return B_wet, W
 
 def net_vertical_load(params, F_z, rho_conc, rho_ballast_wet, rho_water):
